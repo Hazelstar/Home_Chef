592e2caa3b8b777bd739fb1e443a671a28407d5d
marion gayon <mariongayon@gmail.com>
2020-11-16 17:45:07 +0000

    add table availabilities

---
+++ db/migrate/20201116154555_create_availabilities.rb
@@ -0,0 +1,11 @@
+class CreateAvailabilities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :availabilities do |t|
+      t.datetime :start_date
+      t.datetime :end_date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_142509) do
+ActiveRecord::Schema.define(version: 2020_11_16_154555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "availabilities", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_availabilities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +32,14 @@ ActiveRecord::Schema.define(version: 2020_11_16_142509) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "phone_number"
+    t.boolean "is_a_cook"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "availabilities", "users"
 end
