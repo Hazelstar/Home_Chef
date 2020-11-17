dceb455093b42a2bbf08d79cac31d741735ac6b4
Lola Guerin <lola.guerin@gmail.com>
2020-11-17 22:27:02 +0000

    Changes to the user profile as well as redirection after update. work in progress.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users #, controllers: { :registrations => :registrations }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/profile', to: 'pages#profile'
-  # patch '/edit_user_registration', to: 'pages#change_role'
+  patch '/edit_user_registration', to: 'pages#edit_profile'
 
   resources :users do
     resources :bookings, only: [ :new, :create ]
