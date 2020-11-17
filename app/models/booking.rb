d83dd09a97b8841bb7c66e1f156fe09d0bdeb750
Lola Guerin <lola.guerin@gmail.com>
2020-11-17 12:39:55 +0000

    Fixes typo on booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   # belongs_to :user
-  belong_to :cooker, class_name: "user"
+  belongs_to :cooker, class_name: "user"
   belongs_to :booker, class_name: "user"
 
   validates :number_of_meals, :start_date, presence: true
