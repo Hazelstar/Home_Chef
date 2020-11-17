0609d59c4eec63356acfd824187a3449bae674ae
Lola Guerin <lola.guerin@gmail.com>
2020-11-17 16:15:31 +0000

    Changes the validations on the user registration to keep it light, but requires more info on update. Work in progress not finished.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,18 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def profile
+    @user = current_user
+    @user_bookings = Booking.where(user: current_user)
+    @user_availabilities = Availability.where(user: current_user)
+  end
+
+  private
+
+  def change_role
+    current_user.is_a_cook =! current_user.is_a_cook
+    current_user.save
+    redirect_to profile_path(current_user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # has_one_attached :photo # need to activate Cloudinary
 
-  validates :first_name, :last_name, :address, :phone_number, :email, :password, :bio, :is_a_cook, presence: true
+  validates :first_name, :last_name, :address, :phone_number, :email, :password, :bio, :is_a_cook, presence: true, on: :update
   validates :email, :phone_number, uniqueness: true
-  validates :is_a_cook, inclusion: { in: [true, false] }
   validates :bio, length: { in: 50..300 }
   # validates :price, presence: true, numericality: true, if: -> { User.where(is_a_cook: true) }
 
