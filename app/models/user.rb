0587934868402708e007e2081ece70e3d2226399
Lola Guerin <lola.guerin@gmail.com>
2020-11-17 13:13:49 +0000

    Creates User path with Devise controller + views

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ApplicationRecord
   validates :email, :phone_number, uniqueness: true
   validates :is_a_cook, inclusion: { in: [true, false] }
   validates :bio, length: { in: 50..300 }
-  validates :price, presence: true, numericality: true, if: -> { current_user.is_a_cook? }
-  validates :booked, presence: true, if: -> { current_user.is_a_cook? }
+  # validates :price, presence: true, numericality: true, if: -> { User.where(is_a_cook: true) }
 
   has_many :bookings, dependent: :destroy
   has_many :availabilities, dependent: :destroy
