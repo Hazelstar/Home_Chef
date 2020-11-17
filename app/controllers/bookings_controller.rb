477cd5da689fdba069d8ba53bfbc2e517249ee09
Lucien Andrieux <lucien.andrieux@epfedu.fr>
2020-11-17 15:16:43 +0000

    first changes on bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,40 @@
 class BookingsController < ApplicationController
+  # user_bookings POST   /users/:user_id/bookings(.:format)                                                       bookings#create
+  # new_user_booking GET    /users/:user_id/bookings/new(.:format)
+  before_action :set_availability
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(bookings_params)
+    current_user.is_a_cook ? @booking.cooker = current_user : @booking.booker = current_user
+    
+    if @booking.cooker.empty?
+      if time_to_cook(@booking.number_of_meals) > @availability.end_date - @availability.start_date
+        if @booking.save
+          redirect_to users_path(current_user), notice: "New booking from ... to ..."
+        else
+          render :new 
+        end
+      else
+        render :action => :new, :alert => "The time to cook is more than the cooker availabilities for these dates."
+      end
+    end
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:start_date, :number_of_meals, :booker_id, :cooker_id)
+  end
+
+  def time_to_cook(meals)
+    # 10 min preparation time + 30 min per meal
+    10 + 30 * meals
+  end
+
+  def set_availability
+    @availability = Availability.find(params[:user_id])
+  end
 end
