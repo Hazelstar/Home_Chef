177bd2126b03e2cd02386624c1cf0b37385cea0b
Lola Guerin <lola.guerin@gmail.com>
2020-11-18 10:16:18 +0000

    Managed the redirection after sign-in, but fucked up the path to edit/update the user profile -_-

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,6 +19,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  def edit
+    current_user.is_a_cook =! current_user.is_a_cook
+    current_user.save
+    redirect_to profile_path(current_user)
+  end
+
   # PUT /resource
   # def update
   #   super
@@ -38,21 +44,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
-
-  def after_update_path_for(resource)
-    user_path(resource)
-  end
+  private
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
+
+  def after_sign_up_path_for(resource)
+    "/profile"
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    "/newuser"
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users #, controllers: { :registrations => :registrations }
+  devise_for :users, controllers: { :registrations => :registrations }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/profile', to: 'pages#profile'
-  patch '/edit_user_registration', to: 'pages#edit_profile'
+  patch '/edit_user_registration', to: 'registrations#edit'
 
   resources :users do
     resources :bookings, only: [ :new, :create ]
