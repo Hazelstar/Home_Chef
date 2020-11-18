f9b75bd83450f7924e7a85cff735bae4dd2a4b18
Lucien Andrieux <lucien.andrieux@epfedu.fr>
2020-11-18 12:44:08 +0000

    salut

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,8 +2,8 @@
 
 class RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-  after_action :update_cook, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
+  # after_action :update_cook, only: [:update]
   # GET /resource/sign_up
   # def new
   #   super
