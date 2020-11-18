0ca708998e2f0f350972b23cd25765e919e6fe1b
Lucien Andrieux <lucien.andrieux@epfedu.fr>
2020-11-18 19:14:43 +0000

    ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,7 @@
 class BookingsController < ApplicationController
   # user_bookings POST   /users/:user_id/bookings(.:format)                                                       bookings#create
   # new_user_booking GET    /users/:user_id/bookings/new(.:format)
-  before_action :set_availability, :set_cooker
+  before_action :set_cooker_and_availabilities
 
   def new
     @booking = Booking.new
@@ -12,14 +12,10 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.cooker = @cooker
     
-    if bookable?
-      if @booking.save
-        redirect_to profile_path, notice: "New booking from #{@booking.start_date} to #{@booking.start_date + time_to_cook(@booking.number_of_meals)}"
-      else
-        render :new 
-      end
+    if @booking.save
+      redirect_to profile_path, notice: "New booking from #{@booking.start_date} to #{@booking.start_date + time_to_cook(@booking.number_of_meals)}"
     else
-      render :action => :new, :alert => "The time to cook is more than the cooker availabilities for these dates."
+      render :new 
     end
   end
 
@@ -34,17 +30,15 @@ class BookingsController < ApplicationController
     10 + 30 * meals
   end
 
-  def set_availability
-    @availability = Availability.find(@cooker)
-  end
-
   def bookable?
     period_matching = time_to_cook(@booking.number_of_meals) > (@availability.end_date - @availability.start_date)
     same_starting_date = @booking.start_date == @availability.start_date
     period_matching && same_starting_date
   end
 
-  def set_cooker
+  def set_cooker_and_availabilities
     @cooker = User.find(params[:user_id])
+    @availabilities = Availability.where(user: @cooker)
+
   end
 end
