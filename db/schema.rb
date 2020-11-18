116875fbc99150f0dff870e48fb165550ad33df4
marion gayon <mariongayon@gmail.com>
2020-11-18 15:33:40 +0000

    Add dotenv - Protect my secret data in .env file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_11_18_142907) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_142907) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "start_date", default: "2020-11-18 12:00:00"
+    t.datetime "start_date"
     t.index ["user_id"], name: "index_availabilities_on_user_id"
   end
 
