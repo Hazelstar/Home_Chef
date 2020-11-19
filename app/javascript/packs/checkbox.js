474cb1e0f11a1681ee9deae34dad42b8a92f2f7d
Lola Guerin <lola.guerin@gmail.com>
2020-11-19 14:41:03 +0000

    Changes the style of the edit form

--- app/javascript/packs/checkbox.js
+++ app/javascript/packs/checkbox.js
@@ -2,19 +2,17 @@ var checkbox = document.querySelector('input[type="checkbox"]');
 var priceInput = document.querySelector('input[type="integer"]');
 var price_div = document.getElementById('price');
 
-function toggleRequired() {
-  checkbox.onchange = function() {
-     if(this.checked) {
-       price_div.style['display'] = 'block';
-       priceInput.setAttribute('required','required');
-     } else {
-       price_div.style['display'] = 'none';
-       priceInput.removeAttribute('required');
-     }
-  };
-}
+checkbox.onchange = function() {
+   if(this.checked) {
+     price_div.style['display'] = 'block';
+     priceInput.setAttribute('required','required');
+   } else {
+     price_div.style['display'] = 'none';
+     priceInput.removeAttribute('required');
+   }
+};
 
-checkbox.addEventListener('change',toggleRequired,false);
+checkbox.addEventListener('change');
 
 
 export toggleRequired;
