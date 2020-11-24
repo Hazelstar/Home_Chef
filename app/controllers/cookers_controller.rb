476b145d9991782fe9cc9061b1c09568ad6dd799
Lola Guerin <lola.guerin@gmail.com>
2020-11-24 22:39:40 +0000

    Adds a search bar feature in the Cooker Index (for now). Modifies the index action of cooker controller to allow search and creates a scss file to style the search bar.

--- app/controllers/cookers_controller.rb
+++ app/controllers/cookers_controller.rb
@@ -2,14 +2,24 @@ class CookersController < ApplicationController
   before_action :set_counter, only: [:show]
   skip_before_action :authenticate_user!, only: [:index]
 
+  # def index
+  #   @cookers = User.where(is_a_cook: 'true')
+
+  #   respond_to do |format|
+  #     format.html
+  #     format.json { render json: { cookers: @cookers } }
+  #   end
+  # end
+
+  # index with search bar
   def index
     @cookers = User.where(is_a_cook: 'true')
-
-    respond_to do |format|
-      format.html
-      format.json { render json: { cookers: @cookers } }
+    if params[:query].present?
+      @cookers = User.where(first_name: params[:query])
     end
+
   end
+
 
   def show
     @cooker = User.find(params[:id])
