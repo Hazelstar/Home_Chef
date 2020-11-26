72bc46dcae179e2a2dc3f2aac74f0eb9690724bf
Lucien Andrieux <lucien.andrieux@epfedu.fr>
2020-11-26 16:59:53 +0000

    adding favorite_utensil to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:address, :phone_number, :bio, :last_name, :first_name, :password, :email, :is_a_cook, :price, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:address, :phone_number, :bio, :last_name, :first_name, :password, :email, :is_a_cook, :price, :photo, :favorite_utensil])
   end
 
   def default_url_options
