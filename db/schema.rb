5f73c395f2d9ecf264d25dec32e4b6d087543e8a
Lola Guerin <lola.guerin@gmail.com>
2020-12-10 20:17:16 +0000

    Removes price column to users table and add_monetize price_cents to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_125235) do
+ActiveRecord::Schema.define(version: 2020_12_10_201536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,9 +100,8 @@ ActiveRecord::Schema.define(version: 2020_12_10_125235) do
     t.string "phone_number"
     t.string "is_a_cook", default: "false", null: false
     t.text "bio"
-    t.integer "price"
-    t.string "booked", default: "false", null: false
     t.string "favorite_utensil"
+    t.integer "price_cents", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
