802b1abcff01c944cce2e9c29bf961947c7147ce
Lola Guerin <lola.guerin@gmail.com>
2020-12-11 11:38:57 +0000

    Redirects to new user booking path if booking.save unsuccessfull when creating stripe checkout session

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -37,7 +37,6 @@ class BookingsController < ApplicationController
       )
       @booking.update(checkout_session_id: session.id)
       @chatroom = Chatroom.create(name: @booking.cooker.first_name, booking: @booking)
-      raise
       redirect_to booking_path(@booking)
     else
       render :new
